908aa62d2291296003f177be04b2f748aa721ee6
Antonio Jimenez <jimeneznunezantonio@gmail.com>
2019-04-24 12:31:15 +0200

    creada tabla pacientes a falta de nullable en sus atributos

---
+++ app/Paciente.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Paciente extends Model
+{
+    protected $fillable = ['name', 'surname1','surname2','nuhsa','BornDate','weight','height','dni','telephone','email','password','Address','zipCode','passport','nationality','nie'];
+
+
+    public function citas()
+    {
+        return $this->hasMany('App\Cita');
+    }
+
+
+    public function getFullNameAttribute()
+    {
+        return $this->name .' '.$this->surname;
+    }
+    //
+}
