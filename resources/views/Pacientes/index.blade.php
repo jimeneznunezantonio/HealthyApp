1fb0bc10b51d73687bb10fdb9f392c99508ab6ef
Antonio Jimenez <jimeneznunezantonio@gmail.com>
2019-05-28 09:12:12 +0200

    creada tabla pacientes a falta de nullable en sus atributos + controlador paciente casi terminado + register.blade +welcome.blade.php

---
+++ resources/views/Pacientes/index.blade.php
@@ -0,0 +1,77 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 col-md-offset-2">
+                <div class="panel panel-default">
+                    <div class="panel-heading">Pacientes</div>
+
+                    <div class="panel-body">
+                        @include('flash::message')
+                        {!! Form::open(['route' => 'pacientes.create', 'method' => 'get']) !!}
+                        {!!   Form::submit('Crear paciente', ['class'=> 'btn btn-primary'])!!}
+                        {!! Form::close() !!}
+
+                        <br><br>
+                        <table class="table table-striped table-bordered">
+                            <tr>
+                                <th>Nombre</th>
+                                <th>Apellido1</th>
+                                <th>Apellido2</th>
+                                <th>Nuhsa</th>
+                                <th>FechaNacimiento</th>
+                                <th>Peso</th>
+                                <th>Altura</th>
+                                <th>DNI</th>
+                                <th>Teléfono</th>
+                                <th>Email</th>
+                                <th>Contraseña</th>
+                                <th>Dirección</th>
+                                <th>CódigoPostal</th>
+                                <th>Pasaporte</th>
+                                <th>Nacionalidad</th>
+                                <th>NIE</th>
+                                <th colspan="2">Acciones</th>
+                            </tr>
+
+                            @foreach ($pacientes as $paciente)
+
+
+                                <tr>
+                                    <td>{{ $paciente->name }}</td>
+                                    <td>{{ $paciente->surname1 }}</td>
+                                    <td>{{ $paciente->surname2 }}</td>
+                                    <td>{{ $paciente->nuhsa }}</td>
+                                    <td>{{ $paciente->BornDate }}</td>
+                                    <td>{{ $paciente->weight }}</td>
+                                    <td>{{ $paciente->height }}</td>
+                                    <td>{{ $paciente->dni }}</td>
+                                    <td>{{ $paciente->telephone }}</td>
+                                    <td>{{ $paciente->email }}</td>
+                                    <td>{{ $paciente->password }}</td>
+                                    <td>{{ $paciente->address }}</td>
+                                    <td>{{ $paciente->zipCode }}</td>
+                                    <td>{{ $paciente->passport }}</td>
+                                    <td>{{ $paciente->nationality }}</td>
+                                    <td>{{ $paciente->nie }}</td>
+                                    <td>
+                                        {!! Form::open(['route' => ['pacientes.edit',$paciente->nuhsa], 'method' => 'get']) !!}
+                                        {!!   Form::submit('Editar', ['class'=> 'btn btn-warning'])!!}
+                                        {!! Form::close() !!}
+                                    </td>
+                                    <td>
+                                        {!! Form::open(['route' => ['pacientes.destroy',$paciente->nuhsa], 'method' => 'delete']) !!}
+                                        {!!   Form::submit('Borrar', ['class'=> 'btn btn-danger' ,'onclick' => 'if(!confirm("¿Está seguro?"))event.preventDefault();'])!!}
+                                        {!! Form::close() !!}
+
+                                    </td>
+                                </tr>
+                            @endforeach
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
---
+++ resources/views/layouts/welcome.blade.php
@@ -0,0 +1,101 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>HealthyApp</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+
+        <!-- Styles -->
+        <style>
+            html, body {
+                background-color: #cce3f6;
+                color: #636b6f;
+                font-family: 'Nunito', sans-serif;
+                font-weight: 200;
+                height: 100vh;
+                margin: 0;
+            }
+
+            .full-height {
+                height: 100vh;
+            }
+
+            .flex-center {
+                align-items: center;
+                display: flex;
+                justify-content: center;
+            }
+
+            .position-ref {
+                position: relative;
+            }
+
+            .top-right {
+                position: absolute;
+                right: 10px;
+                top: 18px;
+            }
+
+            .content {
+                text-align: center;
+            }
+
+            .title {
+                font-size: 84px;
+            }
+
+            .links > a {
+                color: #636b6f;
+                padding: 0 25px;
+                font-size: 13px;
+                font-weight: 600;
+                letter-spacing: .1rem;
+                text-decoration: none;
+                text-transform: uppercase;
+            }
+
+            .m-b-md {
+                margin-bottom: 30px;
+            }
+        </style>
+    </head>
+    <body>
+        <div class="flex-center position-ref full-height">
+            @if (Route::has('login'))
+                <div class="top-right links">
+                    @auth
+                        <a href="{{ url('/home') }}">Home</a>
+                    @else
+                        <a href="{{ route('login') }}">Login</a>
+
+                        @if (Route::has('register'))
+                            <a href="{{ route('register') }}">Register</a>
+                        @endif
+                    @endauth
+                </div>
+            @endif
+
+            <div class="content">
+                <div class="title m-b-md">
+                    HealthyApp
+                </div>
+                Soy una app que almacena información de la historia clínica de mis pacientes y que ayuda al personal sanitario.<br>
+                <br><div class="links">
+                    <a href="https://laravel.com/docs">Regístrarse</a>
+                    <a href="https://laracasts.com">Iniciar Sesión</a>
+                    <a href="https://laravel-news.com">Ayuda</a>
+                    <a href="https://blog.laravel.com">Más sobre mí</a>
+                    <a href="https://nova.laravel.com">Contacto</a>
+
+                </div><br>
+                Antonio Jiménez Núñez<br>
+                Cristina Morillo Cabezas<br>
+                <br> Somos estudiantes del Grado de Ingeniería de la Salud, concretamente de la mención de Informática Clínica
+            </div>
+        </div>
+    </body>
+</html>
