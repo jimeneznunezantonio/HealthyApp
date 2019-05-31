3a66f1d6c841570dc198861483f54d4ca3f18e82
Antonio Jimenez <jimeneznunezantonio@gmail.com>
2019-05-31 10:04:58 +0200

    VISTA PACIENTE Y ENFERMEDAD y medicamento TERMINADAs!!!!!!!! También creada la clase médico junto con su migración para especialidad

---
+++ app/Especialidad.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Especialidad extends Model
+{
+    protected $fillable = ['name'];
+
+    public function medicos()
+    {
+        return $this->hasMany('App\Medico');
+    }
+
+
+    //
+}
