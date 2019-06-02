1968f1bbb549b4ed091e8631329e8ead82f51248
Antonio Jimenez <jimeneznunezantonio@gmail.com>
2019-06-02 09:57:38 +0200

    Todo hecho menor el edit de tratamiento

---
+++ database/migrations/2019_06_01_102841_create_tratamientos_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTratamientosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tratamientos', function (Blueprint $table) {
+            $table->increments('id');
+            $table->dateTime('start_date');
+            $table->dateTime('end_date');
+            $table->unsignedInteger('medico_id');
+            $table->unsignedInteger('paciente_id');
+            $table->unsignedInteger('medicamento_id');
+            $table->timestamps();
+
+            $table->foreign('medico_id')->references('id')->on('medicos')->onDelete('cascade');
+            $table->foreign('paciente_id')->references('id')->on('pacientes')->onDelete('cascade');
+            $table->foreign('medicamento_id')->references('id')->on('medicamentos')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tratamientos');
+    }
+}
